76b4607dad37e2fb5cd200cfc5418641d3b6ebe1
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-11 17:04:30 -0600

    MVP

--- team-builder/src/Components/MemberList.js
+++ team-builder/src/Components/MemberList.js
@@ -5,7 +5,7 @@ export default function MemberList(props) {
 		<div>
 			{props.memberlist.map((member) => (
 				<div key={member.id}>
-					<h2>Member Info</h2>
+					<h2>Member Contact Info</h2>
 					<p>{member.name}</p>
 					<p>{member.email}</p>
 					<p>{member.role}</p>
